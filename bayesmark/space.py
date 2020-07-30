732b1415b76561ac84165cd87a75abee3085ee0c
David Eriksson <eriksson@uber.com>
2020-07-30 14:22:37 -0700

    Update space.py

--- bayesmark/space.py
+++ bayesmark/space.py
@@ -601,19 +601,19 @@ class Categorical(Space):
         self.dtype = CAT_DTYPE
         # Debatable if decode should go in unwarp or round_to_values
 
-        def _encode(x):
-            return encode(x, self.values, True, WARPED_DTYPE, True)
-
-        def _decode(x):
-            return decode(x, self.values, True)
-
-        self.warp_f = _encode
+        self.warp_f = self._encode
         self.unwarp_f = identity
-        self.round_to_values = _decode
+        self.round_to_values = self._decode
 
         self.lower, self.upper = None, None  # Don't need them
         self.lower_warped = np.zeros(len(values), dtype=WARPED_DTYPE)
         self.upper_warped = np.ones(len(values), dtype=WARPED_DTYPE)
+
+    def _encode(self, x):
+        return encode(x, self.values, True, WARPED_DTYPE, True)
+
+    def _decode(self, x):
+        return decode(x, self.values, True)
 
     def warp(self, X):
         """Warp inputs to a continuous space.
